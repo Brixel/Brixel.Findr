3f39b868c581a836c92b3cc5a27e9c52f4bc7d06
Berend Wouters <berend.wouters@gmail.com>
2020-12-01 20:27:56 +0100

    Some code cleanup

---
+++ Brixel.Findr.API/DTO/GameListDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Brixel.Findr.API.DTO
+{
+    public class GameListDTO
+    {
+        public IReadOnlyList<GameDTO> Games { get; set; }
+        public class GameDTO
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
---
+++ Brixel.Findr.API/DTO/MovePlayerRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Brixel.Findr.API.DTO
+{
+    public class MovePlayerRequestDTO
+    {
+        public Guid GameId { get; set; }
+        public Guid PlayerId { get; set; }
+        public LocationDTO Location { get; set; }
+    }
+}
