c3d0ed94a8adc8657bedca9a57d725e606ab1822
Berend Wouters <berend.wouters@gmail.com>
2020-12-13 21:12:31 +0100

    Move game into modules

--- Brixel.Findr.Angular/brixel-findr/src/app/app.module.ts
+++ Brixel.Findr.Angular/brixel-findr/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameModule } from './game/game.module';
+import { GameModule } from './modules/game/game.module';
 
 @NgModule({
   declarations: [
