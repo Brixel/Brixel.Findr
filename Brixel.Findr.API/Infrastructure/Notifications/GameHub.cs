37f21b5ed95b5bb6f51e3c2bf85a3bc6eb9e90b9
Berend Wouters <berend.wouters@gmail.com>
2021-01-23 20:15:46 +0100

    Adds real-time update of location

---
+++ Brixel.Findr.API/Infrastructure/Notifications/GameHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brixel.Findr.API.Infrastructure.Notifications
+{
+    using Microsoft.AspNetCore.SignalR;
+    using Microsoft.Extensions.Logging;
+
+    public class GameHub : Hub<IGameClient>
+    {
+        private readonly ILogger<GameHub> _logger;
+
+        public GameHub(ILogger<GameHub> logger)
+        {
+            _logger = logger;
+        }
+    }
+
+    public interface IGameClient
+    {
+        Task PlayerMoved(Guid playerId);
+    }
+}
